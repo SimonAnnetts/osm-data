f73670b2586e8e1f69e16286f25ff179dba96677
Simon Annetts <simona@ateb.co.uk>
2018-12-13 10:41:01 +0000

    Calculate disk usage

---
+++ osm_calculate_tile_disk_usage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# calculate how much disk space each zoom level in each map style is using
+
+
+echo "Calculating disk space used. This may take some time!"
+
+find /var/lib/mod_tile -type d  -maxdepth 1 -mindepth 1 |sort |while read b; do
+    for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do 
+        [ -d $b/$i ] && du -s $b/$i | tee -i disk_usage.txt 
+    done;
+done
+
+echo "Done. A copy of this analysis is stored in 'disk_usage.txt'. All your Maps Belong to Us!"

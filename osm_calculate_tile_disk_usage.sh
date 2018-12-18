8cfa0034d7be9ba62d6be89e8c71faff015d7d8c
Simon Annetts <simona@ateb.co.uk>
2018-12-18 10:09:28 +0000

    Store disk usage in disk_usage.txt

--- osm_calculate_tile_disk_usage.sh
+++ osm_calculate_tile_disk_usage.sh
@@ -3,10 +3,11 @@
 
 
 echo "Calculating disk space used. This may take some time!"
+echo "Size     Path" >disk_usage.txt
 
 find /var/lib/mod_tile -type d  -maxdepth 1 -mindepth 1 |sort |while read b; do
     for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do 
-        [ -d $b/$i ] && du -s $b/$i | tee -i disk_usage.txt 
+        [ -d $b/$i ] && du -s $b/$i | tee -ia disk_usage.txt 
     done;
 done
 
